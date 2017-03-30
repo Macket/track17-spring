a67894a7a64658eca9dc0c0c9d2af5e89961030a
Makeev Ivan <makeev@phystech.edu>
2017-03-30 17:06:14 +0300

    Cделал задание Container

--- src/main/java/track/container/Container.java
+++ src/main/java/track/container/Container.java
@@ -1,34 +1,80 @@
 package track.container;
 
-import java.util.List;
+import java.lang.reflect.Field;
+import java.util.*;
 
 import track.container.config.Bean;
+import track.container.config.Property;
+import track.container.config.ValueType;
 
 /**
  * Основной класс контейнера
  * У него определено 2 публичных метода, можете дописывать свои методы и конструкторы
  */
 public class Container {
-
+    HashMap<String, Object> idMap;
+    List<Bean> beans;
 
     // Реализуйте этот конструктор, используется в тестах!
     public Container(List<Bean> beans) {
-
+        this.beans = beans;
+        idMap = new HashMap<String, Object>();
     }
 
     /**
      *  Вернуть объект по имени бина из конфига
      *  Например, Car car = (Car) container.getById("carBean")
      */
-    public Object getById(String id) {
-        return null;
+    public Object getById(String id) throws ReflectiveOperationException, NoSuchElementException {
+        if (idMap.containsKey(id)) {
+            return idMap.get(id);
+        } else {
+
+            // Ищем запрашиваемый кдасс среди бинов. Если не находим, выбрасывается NoSuchElementException
+            Bean bean;
+            Iterator<Bean> iterator = beans.iterator();
+            do {
+                bean = iterator.next();
+            } while (!bean.getId().equals(id));
+
+            // Создаем объект класса и вносим его в обе мапы
+            Class clazz = Class.forName(bean.getClassName());
+            Object obj = clazz.newInstance();
+            idMap.put(bean.getId(), obj);
+
+            // Пробегаемся по всем характеристикам (properties) и устанавливаем нужные значения
+            // для соответствующих полей создаваемого класса
+            Map<String, Property> properties = bean.getProperties();
+            for (Map.Entry<String, Property> entry : properties.entrySet()) {
+                Property property = entry.getValue();
+                Field field = clazz.getDeclaredField(property.getName());
+                field.setAccessible(true);
+                if (property.getType() == ValueType.VAL) {
+                    try {
+                        field.setInt(obj, Integer.parseInt(property.getValue()));
+                    } catch (Exception ex) {
+                        System.out.println("Невозможно создать объект с таким полем");
+                    }
+                } else {
+                    field.set(obj, getById(property.getValue()));
+                }
+            }
+            return obj;
+        }
     }
 
     /**
      * Вернуть объект по имени класса
      * Например, Car car = (Car) container.getByClass("track.container.beans.Car")
      */
-    public Object getByClass(String className) {
-        return null;
+    public Object getByClass(String className) throws ReflectiveOperationException, NoSuchElementException {
+        // Ищем запрашиваемый кдасс среди бинов. Если не находим, выбрасывается NoSuchElementException
+        Bean bean;
+        Iterator<Bean> iterator = beans.iterator();
+        do {
+            bean = iterator.next();
+        } while (!bean.getClassName().equals(className));
+
+        return  getById(bean.getId());
     }
 }
--- src/main/java/track/container/Main.java
+++ src/main/java/track/container/Main.java
@@ -1,12 +1,49 @@
 package track.container;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import track.container.beans.Car;
+import track.container.beans.Engine;
+import track.container.beans.Gear;
+import track.container.config.Bean;
+import track.container.config.InvalidConfigurationException;
+import track.container.config.Root;
+import track.lections.lection4.Item;
+import track.lections.lection4.Person;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.Iterator;
+import java.util.List;
+import java.util.NoSuchElementException;
+
 /**
  *
  */
 public class Main {
 
     public static void main(String[] args) {
+        JsonConfigReader reader = new JsonConfigReader();
+        List<Bean> beans;
+        try {
+            beans = reader.parseBeans(new File("src/main/resources/config.json"));
+            Container container = new Container(beans);
+            Car car = (Car) container.getByClass("track.container.beans.Car");
+            car = (Car) container.getById("carBean");
+            System.out.print("gear_count = " + car.getGear().getCount());
+            System.out.println(", engine_power = " + car.getEngine().getPower());
 
+        } catch (ReflectiveOperationException ex) {
+            System.out.println("Проблемы с рефлекшн");
+        } catch (InvalidConfigurationException ex) {
+            System.out.println("Ошибка чтения конфигурационного файла");
+        } catch (NoSuchElementException ex) {
+            System.out.println("Нет такого бина");;
+        } catch (ClassCastException ex) {
+            System.out.println("Неправильное приведение классов");
+        }
         /*
 
         ПРИМЕР ИСПОЛЬЗОВАНИЯ
