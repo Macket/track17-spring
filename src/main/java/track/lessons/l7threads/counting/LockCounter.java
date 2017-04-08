3850d7875208db049dbe5cde54032b7cd851a5c2
Makeev Ivan <makeev@phystech.edu>
2017-04-08 10:36:29 +0300

    Реализовал блокирующую очередь

--- src/main/java/track/lessons/l7threads/counting/LockCounter.java
+++ src/main/java/track/lessons/l7threads/counting/LockCounter.java
@@ -29,5 +29,4 @@ public class LockCounter implements Counter {
         return counter++;
     }
 
-
 }
--- src/main/java/track/lessons/l7threads/queueu/ListBlockingQueue.java
+++ src/main/java/track/lessons/l7threads/queueu/ListBlockingQueue.java
@@ -9,6 +9,8 @@ public class ListBlockingQueue<E> {
 
     public static final int DEFAULT_CAPACITY = 10;
 
+    private static final Object monitor = new Object();
+
     private int capacity;
     private LinkedList<E> list = new LinkedList<>();
 
@@ -20,26 +22,64 @@ public class ListBlockingQueue<E> {
         this.capacity = capacity;
     }
 
-    public void put(E elem) throws InterruptedException {
-
-        /*
-         Внутри критической секции пытаемся
-         поместить элемент в очередь
-         Если очередь полная isFull==true
-         то блокируемся на wait() пока условие не будет выполнено
-         */
+    /*
+    Внутри критической секции пытаемся
+    поместить элемент в очередь
+    Если очередь полная isFull==true
+    то блокируемся на wait() пока условие не будет выполнено
+    */
+    public synchronized void put(E elem) throws InterruptedException {
+        while (this.isFull()) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        if (this.isEmpty()) {
+            notify();
+        }
+        list.addLast(elem);
     }
 
-    public E take() throws InterruptedException {
+    /*
+    Внутри критической секции пытаемся
+    достать элемент из очереди
+    Если очередь пустая isEmpty == true
+    то блокируемся на wait() пока условие
+    не будет выполнено
+    */
+    public synchronized E take() throws InterruptedException {
+        while (this.isEmpty()) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        if (this.isFull()) {
+            notify();
+        }
+        return list.removeLast();
+    }
 
-        /*
-         Внутри критической секции пытаемся
-         достать элемент из очереди
-         Если очередь пустая isEmpty == true
-         то блокируемся на wait() пока условие
-         не будет выполнено
-         */
-        return null;
+    public synchronized E poll() throws InterruptedException {
+        if (this.isEmpty()) {
+            return null;
+        } else {
+            notify();
+            return list.removeLast();
+        }
+    }
+
+    public synchronized boolean offer(E elem) throws InterruptedException {
+        if (this.isFull()) {
+            return false;
+        } else {
+            list.addLast(elem);
+            notify();
+            return true;
+        }
     }
 
     private boolean isFull() {
@@ -50,4 +90,7 @@ public class ListBlockingQueue<E> {
         return list.size() == 0;
     }
 
+    public int getSize() {
+        return list.size();
+    }
 }
